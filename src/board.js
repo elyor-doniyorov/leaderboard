84562cf5cf07f3d31af6f4820f520a39f837ff4c
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-02 18:01:55 +0500

    Create create game

--- src/board.js
+++ src/board.js
@@ -3,4 +3,25 @@ export default class Board {
     this.id = null;
     this.flag = true;
   }
+  async setId(id) {
+    let tokenId = id.result.substring(id.result.indexOf(':'));
+    tokenId = tokenId.substring(tokenId.lastIndexOf(':') + 1, tokenId.lastIndexOf(' '));
+    this.id = tokenId;
+  }
+
+  getId() {
+    return this.id;
+  }
+
+   createGame=async (name) => {
+     const gameName = { name };
+     const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+     const response = await this.fetchApi(url, 'POST', gameName);
+     if (this.flag === false) {
+       document.getElementById('errorDiv').textContent = 'Bad Request, Please Try Again!!!';
+       setTimeout(() => {
+         document.getElementById('errorDiv').innerHTML = '';
+       }, 3500);
+     } else { this.setId(response); }
+   }
 }
