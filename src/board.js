25592cb6f9bb189e71f24afd634f86a8aec808f0
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-09-02 18:16:05 +0500

    Use single quotes

--- src/board.js
+++ src/board.js
@@ -4,10 +4,10 @@ export default class Board {
     this.flag = true;
   }
   async setId(id) {
-    let tokenId = id.result.substring(id.result.indexOf(":"));
+    let tokenId = id.result.substring(id.result.indexOf(':'));
     tokenId = tokenId.substring(
-      tokenId.lastIndexOf(":") + 1,
-      tokenId.lastIndexOf(" ")
+      tokenId.lastIndexOf(':') + 1,
+      tokenId.lastIndexOf(' ')
     );
     this.id = tokenId;
   }
@@ -19,13 +19,13 @@ export default class Board {
   createGame = async (name) => {
     const gameName = { name };
     const url =
-      "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/";
-    const response = await this.fetchApi(url, "POST", gameName);
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+    const response = await this.fetchApi(url, 'POST', gameName);
     if (this.flag === false) {
-      document.getElementById("errorDiv").textContent =
-        "Bad Request, Please Try Again!!!";
+      document.getElementById('errorDiv').textContent =
+        'Bad Request, Please Try Again!!!';
       setTimeout(() => {
-        document.getElementById("errorDiv").innerHTML = "";
+        document.getElementById('errorDiv').innerHTML = '';
       }, 3000);
     } else {
       this.setId(response);
@@ -33,18 +33,18 @@ export default class Board {
   };
   displayScores = async () => {
     if (this.id !== null) {
-      const scoresList = document.getElementById("ulList");
+      const scoresList = document.getElementById('ulList');
       while (scoresList.firstChild) {
         scoresList.removeChild(scoresList.firstChild);
       }
       const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${encodeURIComponent(
         this.id
       )}/scores/`;
-      const response = await this.fetchApi(url, "GET");
+      const response = await this.fetchApi(url, 'GET');
       response.result.forEach((element) => {
-        const item = document.createElement("li");
-        item.className = "scoresList-items";
-        item.id = "scoresList-items";
+        const item = document.createElement('li');
+        item.className = 'scoresList-items';
+        item.id = 'scoresList-items';
         item.textContent = `${element.user}:${element.score}`;
         scoresList.appendChild(item);
       });
@@ -52,13 +52,13 @@ export default class Board {
   };
 
   addScore = async () => {
-    const name = document.getElementById("name").value;
-    const score = document.getElementById("score").value;
-    if (name === "" || score === "") {
-      document.querySelector(".submitDivErr").textContent =
-        "Please provide a value for game name and score";
+    const name = document.getElementById('name').value;
+    const score = document.getElementById('score').value;
+    if (name === '' || score === '') {
+      document.querySelector('.submitDivErr').textContent =
+        'Please provide a value for game name and score';
       setTimeout(() => {
-        document.querySelector(".submitDivErr").innerHTML = "";
+        document.querySelector('.submitDivErr').innerHTML = '';
       }, 1500);
       return false;
     }
@@ -67,15 +67,15 @@ export default class Board {
         this.id
       )}/scores/`;
       const newScore = {
-        user: document.getElementById("name").value,
-        score: document.getElementById("score").value,
+        user: document.getElementById('name').value,
+        score: document.getElementById('score').value,
       };
-      const response = await this.fetchApi(url, "POST", newScore);
-      if (response.result === "Leaderboard score created correctly.") {
-        const scoresList = document.getElementById("ulList");
-        const item = document.createElement("li");
-        item.className = "scoresList-items";
-        item.id = "scoresList-items";
+      const response = await this.fetchApi(url, 'POST', newScore);
+      if (response.result === 'Leaderboard score created correctly.') {
+        const scoresList = document.getElementById('ulList');
+        const item = document.createElement('li');
+        item.className = 'scoresList-items';
+        item.id = 'scoresList-items';
         item.textContent = `${newScore.user}:${newScore.score}`;
         scoresList.appendChild(item);
       }
@@ -86,10 +86,10 @@ export default class Board {
   fetchApi = async (url, method, jsonBody = null) => {
     const response = await fetch(url, {
       headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Headers": "Content-Type",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "POST,PATCH,OPTIONS,GET",
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST,PATCH,OPTIONS,GET',
       },
       method,
       body: jsonBody !== null ? JSON.stringify(jsonBody) : String.empty,
